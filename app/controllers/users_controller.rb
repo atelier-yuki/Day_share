d646857bf225d077df78fa790f5fcf3c21c46c07
atelier-yuki <atelier.of.timbre.3216ariannol@gmail.com>
2021-01-24 08:56:13 +0900

    詳細ページ遷移設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
 
   def index
   end
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).
+    permit(:nickname, :email, :encrypted_password, :last_name, :first_name, :lname_kana, :fname_kana, :birthday)
+  end
+
 end
